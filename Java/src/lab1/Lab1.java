e93db539ec72c462a6dbdcff8b3930984f4852a7
RVeronica <akinrv16@gmail.com>
2020-09-27 14:42:57 +0300

    lab2 + lab3

--- Java/src/lab1/Lab1.java
+++ Java/src/lab1/Lab1.java
@@ -1,15 +1,18 @@
 package lab1;
+import lab2.Patient;
 
 public class Lab1 {
     public static void main (String[] arg){
- Patient patient1 = new Patient("Rytikova","Female",18);
- Patient patient2 = new Patient("Pchelnik","Male", 30);
-        String k = patient1.getSurname();
-        System.out.println("Show info about patient "+k);
-        patient1.displayInfo();
+        Patient []patients=new Patient[3];
+        patients[0]=new Patient("Rytikov", "Male",22);
+        patients[1]=new Patient("Kolikova", "Female");
+        patients[2]=new Patient("Kotlov",14);
 
-        k = patient2.getSurname();
-        System.out.println("Show info about patient "+k);
-        patient2.displayInfo();
+        for(int i=0;i<3;i++){
+            patients[i].displayInfo();
+        }
+
+        System.out.println("The average age is "+Patient.averageAge(patients));
+        System.out.println("The number of women is "+Patient.women(patients));
     }
 }
